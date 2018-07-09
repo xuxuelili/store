eb68ec0958d3e788e92a533c8e84f955ee9474f0
xuxuelili <2211903076@qq.com>
2018-07-09 15:45:44 +0800

    用户管理-添加用户功能

--- src/views/users/Users.vue
+++ src/views/users/Users.vue
@@ -10,7 +10,29 @@
 <el-input placeholder="请输入内容" class="input-with-select" clearable style="width: 350px;">
 <el-button slot="append" icon="el-icon-search"></el-button>
 </el-input>
-<el-button type="success" plain>添加用户</el-button>
+  <el-button type="success" plain @click="dialogFormVisible = true">添加用户</el-button>
+
+  <el-dialog title="添加用户" :visible.sync="dialogFormVisible">
+    <el-form :model="formData">
+      <el-form-item label="用户名" :label-width="formLabelWidth">
+        <el-input v-model="formData.username" auto-complete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" :label-width="formLabelWidth">
+        <el-input v-model="formData.password" type="password" auto-complete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="邮箱" :label-width="formLabelWidth">
+        <el-input v-model="formData.email" auto-complete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="电话" :label-width="formLabelWidth">
+        <el-input v-model="formData.mobile" auto-complete="off"></el-input>
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="dialogFormVisible = false">取 消</el-button>
+      <el-button type="primary" @click="handleAdd">确 定</el-button>
+    </div>
+  </el-dialog>
+
 </div>
 <!-- 表格 -->
  <el-table
@@ -67,7 +89,15 @@
 export default {
   data() {
     return {
-      list: []
+      list: [],
+      formData: {
+        username: '',
+        password: '',
+        email: '',
+        mobile: ''
+      },
+      dialogFormVisible: false,
+      formLabelWidth: '120px'
     };
   },
   created() {
@@ -79,12 +109,32 @@ export default {
       const token = sessionStorage.getItem('token');
       // 在请求头中设置token
       this.$http.defaults.headers.common['Authorization'] = token;
-      const res = await this.$http.get('users?pagenum=1&pagesize=6');
+      const res = await this.$http.get('users?pagenum=1&pagesize=10');
       // console.log(res);
       const {meta: {msg, status}, data: {users}} = res.data;
       if (status === 200) {
         // 获取数据成功
         this.list = users;
+      } else {
+        this.$message.error(msg);
+      }
+    },
+    async handleAdd() {
+      // 关闭弹出层
+      this.dialogFormVisible = false;
+      // 获取到input的内容
+      this.formData.mg_time = new Date();
+      const res = await this.$http.post('users', this.formData);
+      // console.log(res);
+      const {meta: {msg, status}} = res.data;
+      if (status === 201) {
+        // 创建成功
+        // 清空input
+        this.formData = {};
+        // 刷新页面
+        this.loadData();
+        // 提示信息
+        this.$message.success(msg);
       } else {
         this.$message.error(msg);
       }
